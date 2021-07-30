b8387c529c23abe02377d88b88da22d12102d428
Lesley Sanders <lesleyasanders@gmail.com>
2021-07-30 10:44:34 -0400

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,10 @@
+animal = "cat"
+is_cuddly = true
+animal = "horse"
+num_8 = 8
+num_3 = 24
+quotient = num_3 / num_8
+favorite_films = ["Big Fish", "Infinity War", "Return of the King"]
+favorite_films.shift
+puts favorite_films
+puts animal.length
