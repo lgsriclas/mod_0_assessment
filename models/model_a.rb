89b4648933f0bf3df86f58ea710badd309561528
Lesley Sanders <lesleyasanders@gmail.com>
2021-07-30 10:52:37 -0400

    Add laptop hash

---
+++ models/model_a.rb
@@ -0,0 +1,12 @@
+laptop = {
+  :brand => "Apple",
+  :model => "Macbook Pro",
+  :size_in_inches => 13,
+  :price_in_dollars => 1200,
+  :has_great_battery => true,
+  :has_touch_screen => false,
+  :directories => ["Turing", "Documents", "Pictures"],
+  :files => ["thoughts.md", "reflection.rb", "strings.rb"]
+}
+
+puts laptop
