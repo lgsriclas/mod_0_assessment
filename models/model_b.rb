dab5674c4971b2e92546b209c7621f8537eb3a6d
Lesley Sanders <lesleyasanders@gmail.com>
2021-07-30 10:57:15 -0400

    Add house hash

---
+++ models/model_b.rb
@@ -0,0 +1,12 @@
+house = {
+  :location => "Hoboken",
+  :street => "Garden",
+  :street_address => 727,
+  :apartment_num => 4,
+  :is_a_walkup => true,
+  :has_an_elevator => false,
+  :other_apartments => [1, 2, 3],
+  :neighbors => ["Craig", "Allie", "Christina"]
+}
+
+puts house
